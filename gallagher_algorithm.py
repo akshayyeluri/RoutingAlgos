c94a13a77f17e39f8a2d6c683991768deef71f70
Akshay <ayeluri@caltech.edu>
2020-11-20 13:24:27 -0800

    Add converter and optimization routine

--- gallagher_algorithm.py
+++ gallagher_algorithm.py
@@ -3,39 +3,34 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
+def initializePhi(net):
+    phi = np.empty((net.n, net.n, net.n))
+    paths = dict(nx.all_pairs_shortest_path(net.graph))
+    for j in range(net.n):
+        for i in range(net.n):
+            if i == j:
+                phi[j, i, :] = np.zeros(net.n)
+            else:
+                k = paths[i][j][1]
+                k_vec = np.zeros(net.n)
+                k_vec[k] = 1
+                phi[j, i, :] = k_vec
+    return phi
 
-
-def initializePhi(G):
-	phi = np.empty((G.n, G.n, G.n))
-	paths = dict(nx.all_pairs_shortest_path(G.graph))
-	for j in range(G.n):
-		for i in range(G.n):
-			if i == j:
-				phi[j, i, :] = np.zeros(G.n)
-			else:
-				k = paths[i][j][1]
-				k_vec = np.zeros(G.n)
-				k_vec[k] = 1
-				phi[j, i, :] = k_vec
-	return phi
-
-def phiCheck(phi, G):
-    mask = (G.adj == 0)
+def phiCheck(phi, net):
+    mask = (net.adj == 0)
     cond1 = np.all(phi[:, mask] == 0)
     cond2 = np.all(np.isclose(np.sum(phi, axis=2), 1 - np.eye(len(phi))))
     return (cond1 and cond2)
 
-def calculateMarginals(G, phi, D):
-    dR = np.zeros((G.n, G.n))
-    for j in range(G.n):
+def calculateMarginals(net, phi):
+    D = net.D
+    dR = np.zeros((net.n, net.n))
+    for j in range(net.n):
         destj = nx.from_numpy_matrix(phi[j, :, :], parallel_edges=False, create_using=nx.DiGraph)
         top_sorted = list(reversed(list(nx.topological_sort(destj))))
         for i in top_sorted:
-            for k in range(G.n):
+            for k in range(net.n):
                 dR[j, i] += phi[j, i, k] * (D[i, k] + dR[j, k])
     print(dR)
 
-G = Network(6)
-phi = initializePhi(G)
-D = np.ones((G.n, G.n))
-calculateMarginals(G, phi, D)
--- graphs.py
+++ graphs.py
@@ -1,71 +1,176 @@
 import numpy as np
 import networkx as nx
 import scipy.stats as st
+from IPython import embed
+import matplotlib.pyplot as plt
+
+# TODO: 
+# initialize graph with adjacency matrix
+# fix visualization (directed graph)
+# 
+
+class Converter:
+    def __init__(self, adj):
+        self.n = adj.shape[0]
+        self.mask = self.genMask(adj)
+        self.degrees_of_freedom = np.sum(self.mask)
+
+    def genMask(self, adj):
+        mask0 = adj.copy()
+        mask = np.tile(mask0, (self.n, 1, 1))
+        for i in range(self.n):
+            mask[i, i, :] = False
+        return mask.astype(bool)
+
+    def toPhi(self, flat_arr):
+        phi = np.zeros((self.n, self.n, self.n))
+        phi[self.mask] = flat_arr
+        tots = np.sum(phi, axis=2)
+        tots[np.eye(self.n).astype(bool)] = 1
+        phi = phi / tots[:, :, None]
+        return phi
+
+    def fromPhi(self, phi):
+        return phi[self.mask]
+
 
 class Network:
-    def __init__(self, n, p=0.4, R_gen=np.random.binomial):
+    def __init__(self, n, p=0.4, R_gen=np.random.binomial, seed=7, D=None):
         self.n = n
         self.p = p
+        self.seed = seed
 
-        self.graph = nx.gnp_random_graph(n, p, directed=False)
+        if D is None:
+            D = np.ones((n,n))
+        self.D = D
+
+        inc = 0
+        self.graph = nx.gnp_random_graph(n, p, directed=False, seed=seed)
         while not nx.is_connected(self.graph):
-            self.graph = nx.gnp_random_graph(n, p, directed=False)
+            inc += 1
+            self.graph = nx.gnp_random_graph(n, p, directed=False, seed=seed+inc)
 
-        self.adj = nx.adjacency_matrix(self.graph).todense()
+        self.adj = np.asarray(nx.adjacency_matrix(self.graph).todense())
+        np.random.seed(seed)
         self.R = R_gen(n, p, size=(n,n))
+        self.R[np.eye(n).astype(bool)] = 0 # Make sure no traffic from node to itself
+        self.converter = Converter(self.adj)
+        self.df = self.converter.degrees_of_freedom
 
         self.T = None
         self.F = None
+        self.phi = None
 
 
-    def visualize(self, withEdgeTraffic=False, layout='spring', random_seed=7):
+    def visualize(self, withEdgeTraffic=False, layout='spring', \
+            seed=None, label_pos=0.3, ax=None):
+
+        if ax is None:
+            ax = plt.subplot(111)
+
+        n = self.n
         if layout == 'spring':
+            random_seed = self.seed if seed is None else seed
             pos = nx.spring_layout(self.graph, seed=random_seed)
         elif layout == 'planar':
             pos = nx.planar_layout(self.graph)
 
-        nx.draw_networkx(self.graph, pos=pos)
+        nx.draw_networkx(self.graph, pos=pos, ax=ax)
         if withEdgeTraffic:
-            if self.F is None:
-                self.updateFT()
+            self.updateFT()
             labels = {(i,j): self.F[i,j] for i in range(n) for j in range(n) if self.F[i,j] != 0}
-            nx.draw_networkx_edge_labels(self.graph, pos=pos, edge_labels=labels)
+            nx.draw_networkx_edge_labels(self.graph, pos=pos, ax=ax,\
+                    edge_labels=labels, label_pos=label_pos)
+        return ax
 
 
-    def updateF(self):
-        self.T = getTraffic(self.Phi, self.R)
-        self.F = getF(self.Phi, self.T)
+    def updateFT(self):
+        self.T = getTraffic(self.phi, self.R)
+        self.F = getF(self.phi, self.T)
 
 
-def getTrafficCol(Phi, R, colIdx):
+def getTrafficCol(phi, R, colIdx):
     n = R.shape[0]
     b = R[:, colIdx]
-    A = (np.eye(n) - Phi[colIdx, :, :]) # A_ij = phi_ij(colIdx)
-    if numpy.linalg.det(A) == 0:
-        raise ValueError('LinAlgError caused by invalid Phi!')
+    A = (np.eye(n) - phi[colIdx, :, :]) # A_ij = phi_ij(colIdx)
+    if np.linalg.det(A) == 0:
+        raise ValueError('LinAlgError caused by invalid phi!')
     t = np.linalg.solve(A, b)
     return t
 
 
-def getTraffic(Phi, R):
+def getTraffic(phi, R):
     n = R.shape[0]
     T = np.empty((n,n))
     for i in range(n):
-        T[:, i] = getTrafficCol(Phi, R, i)
+        T[:, i] = getTrafficCol(phi, R, i)
     return T
 
 
-def getF(Phi, T):
+def getF(phi, T):
     n = T.shape[0]
     F = np.empty((n,n))
     for i in range(n):
-        F[i, :] = np.sum(Phi[:, i, :].T @ T[i, :])
+        F[i, :] = phi[:, i, :].T @ T[i, :]
     return F
-    
 
-    
+
+def D_T(network):
+    network.updateFT()
+    return np.sum(network.D * network.F)
+
+
+def obj(phi_flat, network):
+    n = network.n
+    R = network.R
+    D = network.D
+    conv = network.converter
+
+    phi = conv.toPhi(phi_flat)
+    try:
+        T = getTraffic(phi, R)
+    except ValueError as e:
+        return np.inf
+    F = getF(phi, T)
+    return np.sum(D * F)
+
    
-        
+
+############################################################
+# Deprecated
+############################################################
+
+#class Converter:
+#    def __init__(self, adj):
+#        self.n = adj.shape[0]
+#        mask, first = self.genMaskAndFirst(adj)
+#        self.mask = mask
+#        self.first = first
+#        self.degrees_of_freedom = np.sum(self.mask)
+#
+#
+#    def genMaskAndFirst(self, adj):
+#        first_inds = (np.arange(self.n), np.array([np.where(row)[-1][0] for row in adj]))
+#        mask0 = adj.copy()
+#        mask0[first_inds] = False
+#        mask = np.tile(mask0, (self.n, 1, 1))
+#        for i in range(self.n):
+#            mask[i, i, :] = False
+#        return mask.astype(bool), first_inds
+#
+#
+#    def toPhi(self, flat_arr):
+#        phi = np.zeros((self.n, self.n, self.n))
+#        phi[self.mask] = flat_arr
+#        residual = (1 - np.eye(self.n)) - np.sum(phi, axis=2)
+#        for j in range(self.n):
+#            sub_arr = phi[j]
+#            sub_arr[self.first] = residual[j, :]
+#        return phi
+#
+#
+#    def fromPhi(self, phi):
+#        return phi[self.mask]
         
 
 
