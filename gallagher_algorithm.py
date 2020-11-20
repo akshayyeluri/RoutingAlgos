28fe3e7cecdfb2289ca779efa2d9481a00fc6283
rashida-hakim <rhakim@Rashidas-MacBook-Pro.local>
2020-11-19 22:23:21 -0800

    marginal calculation

--- gallagher_algorithm.py
+++ gallagher_algorithm.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
-G = Network(6)
-#G.visualize()
-#plt.show()
+
 
 def initializePhi(G):
 	phi = np.empty((G.n, G.n, G.n))
@@ -21,9 +19,23 @@ def initializePhi(G):
 				phi[j, i, :] = k_vec
 	return phi
 
-def phiCheck(phi):
-	return np.all(np.isclose(np.sum(phi, axis=2), 1 - np.eye(len(phi))))
+def phiCheck(phi, G):
+    mask = (G.adj == 0)
+    cond1 = np.all(phi[:, mask] == 0)
+    cond2 = np.all(np.isclose(np.sum(phi, axis=2), 1 - np.eye(len(phi))))
+    return (cond1 and cond2)
 
-G.Phi = initializePhi(G)
-G.visualize(withEdgeTraffic = True)
-plt.show()
+def calculateMarginals(G, phi, D):
+    dR = np.zeros((G.n, G.n))
+    for j in range(G.n):
+        destj = nx.from_numpy_matrix(phi[j, :, :], parallel_edges=False, create_using=nx.DiGraph)
+        top_sorted = list(reversed(list(nx.topological_sort(destj))))
+        for i in top_sorted:
+            for k in range(G.n):
+                dR[j, i] += phi[j, i, k] * (D[i, k] + dR[j, k])
+    print(dR)
+
+G = Network(6)
+phi = initializePhi(G)
+D = np.ones((G.n, G.n))
+calculateMarginals(G, phi, D)
